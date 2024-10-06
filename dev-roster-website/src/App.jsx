26378a5d29b3fbc499a7ef218be6bb32dec1e67c
Gabriel Sessions <gsess7732@gmail.com>
2024-10-06 18:33:20 -0400

    Added a few teams to the roster

--- dev-roster-website/src/App.jsx
+++ dev-roster-website/src/App.jsx
@@ -4,7 +4,24 @@ function App() {
 
   return (
     <>
-      <h1>JumboCode Team Roster</h1>
+      <h1 className='text-center'>JumboCode Team Roster</h1>
+      <div className='margin-left-1'>
+        <h2>A2Empowerment</h2>
+        <ul>
+          <li>Test Developer Name</li>
+          <li>Test Developer Name</li>
+        </ul>
+        <h2>Bread & Roses</h2>
+        <ul>
+          <li>Test Developer Name</li>
+          <li>Test Developer Name</li>
+        </ul>
+        <h2>Dillar Academy</h2>
+        <ul>
+          <li>Test Developer Name</li>
+          <li>Test Developer Name</li>
+        </ul>
+      </div>
     </>
   )
 }
